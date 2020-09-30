f7c3bfc213f79bdbd71546c81be7f6530084150d
Yasuo Ashina <ashipp3000@gmail.com>
2020-09-30 10:14:26 +0900

    p004-suspense

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
 <template>
-    <h3>Fragmentサポート</h3>
-    <ul>
-      <list-elems/>
-    </ul>
+  <Suspense>
+    <template #default>
+      <async-component1/>
+      <async-component2/>
+    </template>
+    <template #fallback>
+      全コンポーネントの準備完了を待っています...
+    </template>
+  </Suspense>
 </template>
 <script>
-import ListElems from './components/ListElems.vue'
+import AsyncComponent1 from './components/AsyncComponent1.vue'
+import AsyncComponent2 from './components/AsyncComponent2.vue'
 export default {
+  name: 'App',
   components: {
-    ListElems
+    AsyncComponent1,
+    AsyncComponent2
   }
 }
 </script>
---
+++ src/components/AsyncComponent1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="async1">
+    <h3>コンポーネント1</h3>
+    <div>準備完了まで3秒かかります。</div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return new Promise((resolve) => {
+      setTimeout(() => {resolve()}, 3000)
+    })
+    .then(() => {
+      console.log('AsyncComponent1 loaded.');
+      return {}
+    })
+  }
+}
+</script>
+<style scoped>
+#async1 {
+  background-color: #d5d5ff;
+}
+</style>
