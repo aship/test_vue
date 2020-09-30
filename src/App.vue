ab5c7bedcac85257d90306ef1e24b6eb3b26fa41
Yasuo Ashina <ashipp3000@gmail.com>
2020-09-30 10:49:20 +0900

    p006-obj-array-binding-v3

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,60 @@
 <template>
-  <Suspense>
-    <template #default>
-      <async-component1/>
-      <async-component2/>
-    </template>
-    <template #fallback>
-      全コンポーネントの準備完了を待っています...
-    </template>
-  </Suspense>
+  <div>
+    <h3>配列</h3>
+    <ul>
+      <li v-for="(elem, index) in dataArray" v-bind:key="index">
+        {{ index }} : {{ elem }}
+      </li>
+    </ul>
+    <h3>オブジェクト</h3>
+    <div>nameプロパティ：{{ dataObj.name }}</div>
+
+    <h3>配列、オブジェクトの更新</h3>
+    <div>
+      入力値：<input v-model="inputValue">
+    </div>
+    <div>
+      <button @click="updateArrayElem">配列の0番目を更新</button>
+    </div>
+    <div>
+      <button @click="addObjProp">オブジェクトのnameプロパティを追加</button>
+    </div>
+    <div>
+      <button @click="deleteObjProp">オブジェクトのnameプロパティを削除</button>
+    </div>
+  </div>
 </template>
+
 <script>
-import AsyncComponent1 from './components/AsyncComponent1.vue'
-import AsyncComponent2 from './components/AsyncComponent2.vue'
+import { reactive, toRefs } from 'vue'
 export default {
-  name: 'App',
-  components: {
-    AsyncComponent1,
-    AsyncComponent2
+  setup() {
+    const state = reactive({
+      inputValue : '',
+      dataArray: ['iPhone 11', 'Galaxy S20 5G'],
+      dataObj: {'name': 'AQUOS R5G'}
+    });
+    // 配列要素を更新
+    function updateArrayElem() {
+      state.dataArray[0] = state.inputValue;
+    }
+    // オブジェクトのプロパティを追加
+    function addObjProp() {
+      state.dataObj.name = state.inputValue
+    }
+    // オブジェクトのプロパティを削除
+    function deleteObjProp() {
+      delete state.dataObj.name
+    }
+    return {
+      ...toRefs(state),
+      updateArrayElem, addObjProp, deleteObjProp
+    }
   }
 }
 </script>
+<style scoped>
+button {
+  margin-top: 5px;
+}
+</style>
