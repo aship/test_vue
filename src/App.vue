89d27280ab1dedddc0a3114f62c25107d5b9615d
Yasuo Ashina <ashipp3000@gmail.com>
2020-10-01 14:13:30 +0900

    p002-teleport

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,16 @@
 <template>
   <div>
-    <h3>複数v-modelを利用したフォーム</h3>
-    <!-- 複数v-model設定 -->
-    <multi-v-model v-model:name="name" v-model:vendor="vendor"/>
-    <h3>フォーム入力内容</h3>
-    <div>機種名：{{ name }}</div>
-    <div>メーカー：{{ vendor }}</div>
+    <teleport-from></teleport-from>
   </div>
 </template>
+
 <script>
-import { ref } from 'vue'
-import MultiVModel from './components/MultiVModel.vue'
+import TeleportFrom from './components/TeleportFrom.vue'
+
 export default {
+  name: 'App',
   components: {
-    MultiVModel
-  },
-  setup() {
-    const name = ref('Galaxy S20 5G')
-    const vendor = ref('Samsung')
-    return {
-      name, vendor
-    }
+    TeleportFrom
   }
 }
 </script>
