25ab25c268b4b3517556a4dfc6ceec5f4fcda2f8
Yasuo Ashina <ashipp3000@gmail.com>
2020-10-01 14:10:18 +0900

    p001-multi-vmodel

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <one-phone name="Galaxy S20 5G" vendor="Samsung" v-bind:weight="163"/>
+    <h3>複数v-modelを利用したフォーム</h3>
+    <!-- 複数v-model設定 -->
+    <multi-v-model v-model:name="name" v-model:vendor="vendor"/>
+    <h3>フォーム入力内容</h3>
+    <div>機種名：{{ name }}</div>
+    <div>メーカー：{{ vendor }}</div>
   </div>
 </template>
 <script>
-import OnePhone from './components/OnePhone.vue'
+import { ref } from 'vue'
+import MultiVModel from './components/MultiVModel.vue'
 export default {
-  name: 'App',
   components: {
-    OnePhone
+    MultiVModel
+  },
+  setup() {
+    const name = ref('Galaxy S20 5G')
+    const vendor = ref('Samsung')
+    return {
+      name, vendor
+    }
   }
 }
 </script>
---
+++ src/components/MultiVModel.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="multi-v-model">
+    <div>
+      <div>機種名：</div>
+      <input v-bind:value="name" v-on:input="updateName($event.target.value)">
+    </div>
+    <div>
+      <div>メーカー：</div>
+      <input v-bind:value="vendor" v-on:input="updateVendor($event.target.value)">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  // v-modelディレクティブで指定されるプロパティ
+  props: {
+    name: String,
+    vendor: String
+  },
+  // コンポーネント設定処理
+  setup(props, context) {
+    // name変更時の処理
+    function updateName(value) {
+      context.emit('update:name', value) // 変更イベントを発生
+    }
+    // vendor変更時の処理
+    function updateVendor(value) {
+      context.emit('update:vendor', value) // 変更イベントを発生
+    }
+    return {
+      updateName, updateVendor
+    }
+  }
+}
+</script>
+<style scoped>
+.multi-v-model {
+  background-color: #d5d5ff;
+}
+</style>
